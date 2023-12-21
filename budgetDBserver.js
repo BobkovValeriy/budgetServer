0b3e54a61e54a4e2a69f14a3f5318777dfde54a6
BobkovValeriy <darkmito17051990@gmail.com>
2023-12-21 22:37:46 +0200

    minusNoDemon

--- budgetDBserver.js
+++ budgetDBserver.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URL || 5000, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB!');
     })
